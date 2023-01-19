4dbb7b74dcc2c0017666aef246c8c1d36ea84f00
truemabadi <kaba4ok.den@gmail.com>
2023-01-20 01:16:00 +0300

    Initial commit

---
+++ src/main/java/ru/hogwarts/fkngschool/FkngschoolApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.fkngschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FkngschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FkngschoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hogwarts/fkngschool/FkngschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.fkngschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FkngschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
